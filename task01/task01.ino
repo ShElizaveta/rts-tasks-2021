736fd4f60e331dea33cc823f6fefd050a1b1d08e
ShElizaveta <86772447+ShElizaveta@users.noreply.github.com>
2021-11-27 15:36:15 +0400

    Update task01.ino

--- task01/task01.ino
+++ task01/task01.ino
@@ -3,23 +3,27 @@ const int ledPin_2 = 6;
 const int ledPin_3 = 11;
 const int ledPin_4 = 10;
 const int ledPin_5 = 9;
+const int ledPin_6 = 3;
 
 unsigned long next_time_1; 
 unsigned long next_time_2; 
 unsigned long next_time_3; 
 unsigned long next_time_4; 
 unsigned long next_time_5; 
-unsigned long timeout_1 = 1000000; 
+unsigned long next_time_6;
+unsigned long timeout_1 = 250000; 
 unsigned long timeout_2 = 750000; 
-unsigned long timeout_3 = 500000;
+unsigned long timeout_3 = 550000;
 unsigned long timeout_4 = 250000;
-int timeout_5 = 250;
+unsigned long timeout_5 = 200000;
+unsigned long timeout_6 = 250000;
 
 int led_state_1 = 0; 
 int led_state_2 = 0; 
 int led_state_3 = 0;
 int led_state_4 = 0;
 int led_state_5 = 0;
+int led_state_6 = 0;
 
 void setup() {
   pinMode(ledPin_1, OUTPUT); 
@@ -27,18 +31,21 @@ void setup() {
   pinMode(ledPin_3, OUTPUT);
   pinMode(ledPin_4, OUTPUT);
   pinMode(ledPin_5, OUTPUT);
+  pinMode(ledPin_6, OUTPUT);
 
   digitalWrite(ledPin_1, led_state_1); 
   digitalWrite(ledPin_2, led_state_2); 
   digitalWrite(ledPin_3, led_state_3);
   digitalWrite(ledPin_4, led_state_4);
   digitalWrite(ledPin_5, led_state_5);
+  digitalWrite(ledPin_6, led_state_6);
 
   next_time_1 = micros() + timeout_1; 
   next_time_2 = micros() + timeout_2; 
   next_time_3 = micros() + timeout_3;
   next_time_4 = micros() + timeout_4;
-  next_time_5 = millis() + timeout_5;
+  next_time_5 = micros() + timeout_5;
+  next_time_6 = micros() + timeout_6;
 }
 
 void loop() {
@@ -70,11 +77,16 @@ void loop() {
     digitalWrite(ledPin_4, led_state_4);
   }
 
-  now_time = millis();
+  now_time = micros();
   if (now_time >= next_time_5) {
     next_time_5 = now_time + timeout_5;
     led_state_5 = !led_state_5;
     digitalWrite(ledPin_5, led_state_5);
   }
-
+now_time = micros();
+  if (now_time >= next_time_6) {
+    next_time_6 = now_time + timeout_6;
+    led_state_6 = !led_state_6;
+    digitalWrite(ledPin_6, led_state_6);
+  }
 }
